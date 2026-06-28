372e09084925147eed9dfb78d265df81fd05572b
Md Sajid <sajidcse202024@gmail.com>
2026-06-29 00:57:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const count = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (count[str[i]]) {
+      count[str[i]]++;
+    } else {
+      count[str[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (count[str[i]] === 1) {
+      return str[i];
+    }
+  }
+
+  return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
